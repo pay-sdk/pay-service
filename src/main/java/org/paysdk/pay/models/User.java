48b30e9c4a57f323cdb3c8d06073f500ee52e552
lmaridae <lmaridae@icloud.com>
2020-11-14 16:51:17 +0300

    fixed user entity

--- src/main/java/org/paysdk/pay/models/User.java
+++ src/main/java/org/paysdk/pay/models/User.java
@@ -1,7 +1,9 @@
 package org.paysdk.pay.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
